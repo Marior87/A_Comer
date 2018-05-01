25f60e01ac38a67c3ef8b1c61659038d9eab9673
Stefano Lagattolla <stefanolagattolla.s@gmail.com>
2018-05-01 11:57:05 -0400

    Ultimo commit

--- js/index.js
+++ js/index.js
@@ -1,31 +1,4 @@
-var selector = document.getElementById("slct");
-var contPrincipal = document.getElementById("principal");
-function seleccion(selector) {
 
-switch(selector.selectedIndex) {
-    case 0:
-        contPrincipal.style.backgroundImage = "url('css/Maracaibo.png')";
-        break;
-    case 1:
-        contPrincipal.style.backgroundImage = "url('css/Maracaibo.png')";
-        break;
-    case 2:
-        contPrincipal.style.backgroundImage = "url(css/Caracas.png)";
-        break;
-    case 3:
-        contPrincipal.style.backgroundImage = "url('css/SanCristobal.png')";
-        break;
-    case 4:
-        contPrincipal.style.backgroundImage = "url('css/Barquisimeto.png')";
-        break;
-    case 5:
-        contPrincipal.style.backgroundImage = "url('css/Margarita.png')";
-        break;
-    default:
-        contPrincipal.style.backgroundImage = "url('Maracaibo.png')";
-        break;
-}
-}
 //////////////////////////////////////////////////////////////
 var menu = document.getElementById('checkMenu');
 var nav = document.getElementById('nav');
