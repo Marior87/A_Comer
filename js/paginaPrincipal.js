303879fbfac56ba370628958fa330f865bb2b933
Stefano Lagattolla <stefanolagattolla.s@gmail.com>
2018-05-21 22:26:11 -0400

    Actualizacion de la pagina con Vue.js

---
+++ js/paginaPrincipal.js
@@ -0,0 +1,74 @@
+var json =  [{
+    nombre: 'Mi Ternerita Norte',
+    tipo: 'Churrasqueria, Tex-mex',
+    precio: 4,
+    img: 'img/paginaPrincipal/Steak.png',
+    tipoRestaurante: 1
+},
+{
+    nombre: 'Bon Burger',
+    tipo: 'Hamburguesas',
+    precio: 4,
+    img: 'img/paginaPrincipal/Rest1.jpg',
+    tipoRestaurante: 2 //Comida Rapida = 2
+},
+{
+    nombre: 'Gula Coffee & Bistro',
+    tipo: 'Cafe, Bistro',
+    precio: 3,
+    img: 'img/paginaPrincipal/Gula.png',
+    tipoRestaurante: 3 //Cafe y Postres = 3
+},
+{
+    nombre: 'Cuenta Trez',
+    tipo: 'Bar, Pizzeria',
+    precio: 3,
+    img: 'img/paginaPrincipal/Rest2.jpg',
+    tipoRestaurante: 1 //Restaurante Fancy = 1
+},
+{
+    nombre: 'Super Arepa',
+    tipo: 'Comida Rápida, Arepa',
+    precio: 2,
+    img: 'img/paginaPrincipal/superArepa.png',
+    tipoRestaurante: 2
+}
+]
+
+
+let app1 = new Vue({
+    el: '#app-1',
+    data: {
+        mejoresRestaurantes:   json
+        },
+    methods: {
+        newWindow: function(){
+            window.open('./../restaurante.html')
+        }
+    }
+
+})
+let app2 = new Vue({
+    el: "#app-2",
+    data: {
+        mejoresRestaurantes: json
+    },
+    methods: {
+        newWindow: function(){
+            window.open('./../restaurante.html')
+        }
+    }
+})
+let app3 = new Vue({
+    el: "#app-3",
+    data: {
+        mejoresRestaurantes: json
+    },
+    methods: {
+        newWindow: function(){
+            window.open('./../restaurante.html')
+        }
+    }
+})
+
+
